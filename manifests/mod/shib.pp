fcdbde3b32a444a247a345fccd661c27af8c64ce
Guy Vandenbergh <gvdb1967@gmail.com>
2016-02-29 11:59:29 +0100

    require shib. package

--- manifests/mod/shib.pp
+++ manifests/mod/shib.pp
@@ -1,10 +1,20 @@
 class apache::mod::shib (
   $suppress_warning = false,
   $mod_full_path = undef,
+  $version,
+  $repo
 ) {
   $mod_shib = 'shib2'
 
   if ($::osfamily == 'RedHat' or $::osfamily == 'CentOS') and ! $suppress_warning {
+    package {
+      'shibboleth':
+        ensure          => $version,
+        install_options => [{
+          '--enablerepo' => $repo,
+        }]
+    }
+    notify {"shibboleth version: ${version}":}
     # RedHat distributions do not have Apache mod_shib in their default package repositories
     # use mod_full_path to set the full path for the shibboleth module
     if ($mod_full_path == undef) {
